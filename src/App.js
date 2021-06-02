afd9d364d75c556a9f79a12dfeb5fb0cbcc66f9c
boukramalika <bouhedjarmalika92@gmail.com>
2021-06-02 17:25:34 +0200

    partie 2 elsy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import React from "react";
+import './styles/global.css';
 import './App.css';
+import Box from './components/Box.jsx';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const tempMin = -20
+const tempMax = 40
+const heartMin = 80
+const heartMax = 180
+const stepsMin = 0
+const stepsMax = 50000
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      water:1.5, 
+      heart: 120, 
+      temperature: -10, 
+      steps:3000
+    };
+  }
+  onStepsChange = (e) => {
+    this.setState({ steps: e.target.value });
+  };
+  onHeartChange = (e) => {
+    this.setState({ heart: e.target.value });
+  };
+    onTempChange = (e) => {
+    this.setState({ temp: e.target.value });
+  };
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <Box icon="local_drink" color="#3A85FF" value={1.5} unit="L" />
+          <Box
+            icon="directions_walk"
+            color="black"
+            value={this.state.steps}
+            unit="steps"
+            min={stepsMin}
+            max={stepsMax}
+            onChange={this.onStepsChange}
+          />
+          <Box icon="favorite" color="red" value={this.state.heart} unit="bpm" min ={heartMin}  max={ heartMax }  onChange={this.onHeartChange}
+          />
+          <Box icon="wb_sunny" color="yellow" value={this.state.temp} unit="°C" min={tempMin } max={tempMax }  onChange={this.onTempChange}/>
+
+        </div>
+      </div>
+    );
+  }
 }
-
 export default App;
+
---
+++ src/components/Box.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import '../App.css';
+class Box extends React.Component {
+    render() {
+        let slider
+        if (this.props.icon !== "local_drink") {
+          slider = <input type="range" min={this.props.min}  max={this.props.max} onChange={this.props.onChange}   value={this.props.value}></input>
+        } else { slider = null }
+ 
+        return (
+            <div className="box col-sm-3 col-6">
+                <span className="material-icons"
+                    style={{ fontSize: 100, color: this.props.color }}>
+                    {this.props.icon}</span>
+                <p>{this.props.value} {this.props.unit}</p>
+                <div>
+                    {slider}
+                </div>
+            </div>
+        )
+    }
+}
+export default Box;
